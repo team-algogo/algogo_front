46c1ef764aee7478e0a0cc169f478e0ae643a782
bye-on <codnjs842@naver.com>
2025-11-29 19:15:23 +0900

    feat: Card Components

---
+++ frontend/src/components/cards/list/ListCard.tsx
@@ -0,0 +1,20 @@
+import Badge from "../../badge/Badge";
+import TextLink from "../../textLink/TextLink";
+
+const ListCard = () => {
+  return (
+    <div className="flex flex-col w-[663px] px-6 py-4 gap-y-2 border-2 border-grayscale-warm-gray rounded-lg">
+      <div className="text-title">Title</div>
+      <div>contnet</div>
+      <div className="flex gap-3">
+        <Badge variant="white">badge</Badge>
+        <TextLink src="#">
+          <img src="/icons/bookIcon.svg" className="size-4" />
+          Label
+        </TextLink>
+      </div>
+    </div>
+  );
+};
+
+export default ListCard;

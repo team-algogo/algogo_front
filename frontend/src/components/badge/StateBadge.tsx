5f3f6041eaf8b1dbee5ea8764ffaba1114e5ed26
bye-on <codnjs842@naver.com>
2025-11-29 16:39:11 +0900

    chore: State Badge 내부 텍스트 정렬 변경

--- frontend/src/components/badge/StateBadge.tsx
+++ frontend/src/components/badge/StateBadge.tsx
@@ -1,14 +1,16 @@
 interface BadgeProps {
-    isPassed: boolean;
-    hasText: boolean;
+  isPassed: boolean;
+  hasText: boolean;
 }
 
 const StateBadge = ({ isPassed, hasText }: BadgeProps) => {
-  const bgColor = isPassed ? 'bg-primary-main' : 'bg-alert-error';
+  const bgColor = isPassed ? "bg-primary-main" : "bg-alert-error";
 
   return hasText ? (
-    <div className={`${bgColor} text-white text-center h-6 w-15 px-3 py-0.5 rounded-lg`}>
-      {isPassed ? '통과' : '실패'}
+    <div
+      className={`${bgColor} text-white flex justify-center items-center h-6 w-15 px-3 py-0.5 rounded-lg`}
+    >
+      {isPassed ? "통과" : "실패"}
     </div>
   ) : (
     <div className={`${bgColor} w-4 h-4 rounded-full`} />

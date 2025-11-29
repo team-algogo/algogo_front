1873f4359ce5ab95a227d0a96446b1c7c7954f46
bye-on <codnjs842@naver.com>
2025-11-29 16:39:48 +0900

    feat: StatisticsTable Component

--- frontend/src/components/table/StatisticsTable.tsx
+++ frontend/src/components/table/StatisticsTable.tsx
@@ -1,26 +1,26 @@
-import StaticsTableItem from "./StatisticsTableItem"
+import StatisticsTableItem from "./StatisticsTableItem";
 
-const StaticsTable = () => {
-    return (
-        <table>
-            <thead className="border-b-2 border-b-primary-400">
-                <tr >
-                    <th className="px-6 py-3">순위</th>
-                    <th className="px-6 py-3">사용자</th>
-                    <th className="px-6 py-3">결과</th>
-                    <th className="px-6 py-3">메모리</th>
-                    <th className="px-6 py-3">시간</th>
-                    <th className="px-6 py-3">언어</th>
-                    <th className="px-6 py-3">제출일</th>
-                    <th className="px-6 py-3">문제풀기</th>
-                    <th className="px-6 py-3">리뷰</th>
-                </tr>
-            </thead>
-            <tbody>
-                <StaticsTableItem />
-            </tbody>
-        </table>
-    )
-}
+const StatisticsTable = () => {
+  return (
+    <table>
+      <thead className="border-b-2 border-b-primary-400">
+        <tr>
+          <th className="px-6 py-3">순위</th>
+          <th className="px-6 py-3">사용자</th>
+          <th className="px-6 py-3">결과</th>
+          <th className="px-6 py-3">메모리</th>
+          <th className="px-6 py-3">시간</th>
+          <th className="px-6 py-3">언어</th>
+          <th className="px-6 py-3">제출일</th>
+          <th className="px-6 py-3">문제풀기</th>
+          <th className="px-6 py-3">리뷰</th>
+        </tr>
+      </thead>
+      <tbody>
+        <StatisticsTableItem />
+      </tbody>
+    </table>
+  );
+};
 
-export default StaticsTable;
+export default StatisticsTable;

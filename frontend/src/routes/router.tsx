b14cacaeab786f6e144d7b29b2343f8a428f7b68
bye-on <codnjs842@naver.com>
2025-12-02 21:21:40 +0900

    feat: router 설정

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
+import MainPage from "../pages/main/MainPage";
+import LoginPage from "../pages/login/LoginPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [],
+    children: [
+      { index: true, element: <MainPage /> },
+      { path: "login", element: <LoginPage /> },
+    ],
   },
 ]);
 

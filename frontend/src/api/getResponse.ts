6ce69498f0b76707742a01db5422e8c733339433
Teddysir <sdzx0719@naver.com>
2025-12-09 20:48:12 +0900

    fix: fixed untracked gitignore

--- frontend/src/api/getResponse.ts
+++
@@ -1,9 +0,0 @@
-import type Response from "../type/response";
-import client from "./client";
-
-const getResponse = async <T>(url: string) => {
-  const response = await client.get<Response<T>>(url);
-  return response.data;
-};
-
-export default getResponse;
